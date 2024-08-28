b469915cf124c2e08adb6e5e241a9ab7bffaeff4
David Griffiths <davidgriffiths@hotmail.com>
2024-08-28 08:47:40 +0100

    message service

--- app/Services/AppSettingService.php
+++ app/Services/AppSettingService.php
@@ -2,49 +2,15 @@
 
 namespace App\Services;
 
-use App\Models\AppSetting;
+use App\Models\Message;
 use Illuminate\Support\Facades\Cache;
 
-class AppSettingService
+class MessageService
 {
-    public function get(string $key, $default = null)
+    public function thing(string $key, $default = null)
     {
-        // TODO: decide on cache shizzle
-        // return Cache::remember("app_setting_{$key}", 3600, function () use ($key, $default) {
-            $setting = AppSetting::where('key', $key)->first();
-            return $setting ? $this->castValue($setting->value, $setting->type) : $default;
-        // });
+        // Blah
     }
 
-    public function set(string $key, $value, string $type = 'string', string $description = null)
-    {
-        $setting = AppSetting::updateOrCreate(
-            ['key' => $key],
-            [
-                'value' => (string) $value,
-                'type' => $type,
-                'description' => $description
-            ]
-        );
-
-        Cache::put("app_setting_{$key}", $this->castValue($setting->value, $setting->type), 3600);
-
-        return $setting;
-    }
-
-    private function castValue($value, string $type)
-    {
-        switch ($type) {
-            case 'boolean':
-                return filter_var($value, FILTER_VALIDATE_BOOLEAN);
-            case 'int':
-                return (int) $value;
-            case 'float':
-                return (float) $value;
-            case 'json':
-                return json_decode($value, true);
-            default:
-                return $value;
-        }
-    }
+    
 }
---
+++ app/Services/MessageService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Message;
+use Illuminate\Support\Facades\Cache;
+
+class MessageService
+{
+    public function blah(string $key, $default = null)
+    {
+        // TODO: Use this to create the user and the conversation
+        // before sending etc
+            
+    }
+
+    
+}
