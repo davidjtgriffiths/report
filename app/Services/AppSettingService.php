aafd6b7ae6cf9859611950ce311141cdca0ea8fe
David Griffiths <davidgriffiths@hotmail.com>
2024-08-29 06:59:51 +0100

    oops

--- app/Services/AppSettingService.php
+++ app/Services/AppSettingService.php
@@ -2,15 +2,45 @@
 
 namespace App\Services;
 
-use App\Models\Message;
+use App\Models\AppSetting;
 use Illuminate\Support\Facades\Cache;
 
-class MessageService
+class AppSettingService
 {
-    public function thing(string $key, $default = null)
+    public function get(string $key, $default = null)
     {
-        // Blah
+        return Cache::remember("app_setting_{$key}", 3600, function () use ($key, $default) {
+            $setting = AppSetting::where('key', $key)->first();
+            return $setting ? $this->castValue($setting->value, $setting->type) : $default;
+        });
     }
 
-    
+    public function set(string $key, $value, string $type = 'string', string $description = null)
+    {
+        $setting = AppSetting::updateOrCreate(
+            ['key' => $key],
+            [
+                'value' => (string) $value,
+                'type' => $type,
+                'description' => $description
+            ]
+        );
+        Cache::put("app_setting_{$key}", $this->castValue($setting->value, $setting->type), 3600);
+        return $setting;
+    }
+    private function castValue($value, string $type)
+    {
+        switch ($type) {
+            case 'boolean':
+                return filter_var($value, FILTER_VALIDATE_BOOLEAN);
+            case 'int':
+                return (int) $value;
+            case 'float':
+                return (float) $value;
+            case 'json':
+                return json_decode($value, true);
+            default:
+                return $value;
+        }
+    }
 }
